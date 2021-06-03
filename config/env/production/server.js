776f8e67014543d303d0b881ae273472cc632195
aelf-hz <mason.huang@aelf.io>
2021-06-03 17:20:17 +0800

    refactor: 💡 update

---
+++ config/env/production/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 9910),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5e6bdea11dd0ac7954fe4fcb800b9bde'),
+    },
+  },
+});
