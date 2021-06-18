1291d80127949ff57c2c7f357d74f61ddf8928cf
aelf-hz <mason.huang@aelf.io>
2021-06-18 11:25:20 +0800

    refactor: 💡 preview

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,11 @@
+const env = process.env.TERMITE_ENV;
+const port = env === "preview" ? 9911 : 9910;
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 9910),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", port),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '5e6bdea11dd0ac7954fe4fcb800b9bde'),
+      secret: env("ADMIN_JWT_SECRET", "5e6bdea11dd0ac7954fe4fcb800b9bde"),
     },
   },
 });
